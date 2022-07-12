4da1d94ae8859dd483686669e015788220626a89
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-07-12 14:16:46 -0600

    Created the many to many table, FK indexs

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -67,3 +67,19 @@ ADD FOREIGN KEY (invoice_id) REFERENCES invoices(id);
 
 ALTER TABLE invoice_items
 ADD FOREIGN KEY (treatment_id) REFERENCES treatments(id);
+
+-- Create the table linking medical_histories and treatments many to many
+CREATE TABLE medical_histories_treatments (
+medical_histories_id INT,
+treatments_id INT,
+CONSTRAINT fk_medical_histories FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id),
+CONSTRAINT fk_treatments FOREIGN KEY (treatment_id) REFERENCES treatments(id)
+);
+
+--Create indexes to be able to get data very faster from the database
+CREATE INDEX medical_history_patient_id ON medical_histories (patient_id);
+CREATE INDEX invoices_medical_history_id ON invoices (medical_history_id);
+CREATE INDEX invoice_items_invoice_id ON invoice_items (invoice_id);
+CREATE INDEX invoice_items_treatments_id_index ON invoice_items(treament_id);
+CREATE INDEX medical_history_and_treatment_medical_history_index ON medical_histories_treaments (medical_history_id);
+CREATE INDEX medical_history_treaments_treament_id_index ON medical_histories_treaments(treament_id);
